ca23fc9cf05001a8574f5be22fbd7abec8a23705
DARSHAN <darshangangwaldeveloper@gmail.com>
2024-11-07 23:46:01 +0530

    code updated in delete file

--- src/controllers/invoice/delete.ts
+++ src/controllers/invoice/delete.ts
@@ -17,6 +17,7 @@ const deleteWrapper: RequestHandler = async (req, res) => {
   console.log(id)
   console.log(id)
   console.log(id)
+  console.log(id)
   
   if (!isValidObjectId(id)) {
     return res.status(400).json({ error: 'Invalid invoice ID' });
